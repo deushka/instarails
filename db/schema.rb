715ae545f4b97a333562c16c2256db37116c5089
Yersultan Nadyrmagambet <nadyrcoop@gmail.com>
2018-06-18 14:04:47 +0600

    Migrate fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_17_045002) do
+ActiveRecord::Schema.define(version: 2018_06_15_093003) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
