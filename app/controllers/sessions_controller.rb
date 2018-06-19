43ba81a58b7664060a32ef0e9706c9c4c3d4101e
Yersultan Nadyrmagambet <nadyrcoop@gmail.com>
2018-06-19 14:41:03 +0600

    Fixed some things

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,6 @@
 class SessionsController < ApplicationController
 
 	def new
-<<<<<<< HEAD
-=======
-
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 	end
 
 	def create
@@ -14,21 +10,12 @@ class SessionsController < ApplicationController
 		else
 			user = User.find_by_username(login)
 		end
-<<<<<<< HEAD
-=======
-
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
 		else
-<<<<<<< HEAD
 			render "new"
 			flash[:error] = "Invalid username or password"
-=======
-			flash[:error] = "Invalid username or password"
-			render "new"
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 		end
 	end
 
@@ -38,7 +25,3 @@ class SessionsController < ApplicationController
 	end
 
 end
-<<<<<<< HEAD
-
-=======
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,4 @@
 module SessionsHelper
-<<<<<<< HEAD
-	
-=======
-
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 	def sign_in(user)
 		session[:user_id] = user.id
 	end
@@ -11,27 +6,19 @@ module SessionsHelper
 	def signed_in?
 		!current_user.nil?
 	end
-
-<<<<<<< HEAD
 	def sign_out
 		session[:user_id] = nil
 		@current_user = nil
 	end
 
-=======
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 	def current_user
 		if session[:user_id]
 			@current_user ||= User.find(session[:user_id])
 		end
 	end
 
-<<<<<<< HEAD
-=======
 	def sign_out
 		session[:user_id] = nil
 		@current_user = nil
 	end
-
->>>>>>> 323ab7533b86b961091794cd083e47830b2ae140
 end
