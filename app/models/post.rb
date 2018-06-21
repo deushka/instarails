c62e3b0abb85b9e31fd9995dc1ef046e3c817ff7
Yersultan Nadyrmagambet <nadyrcoop@gmail.com>
2018-06-21 15:49:43 +0600

    Edit partially added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
 	validates :body, length: {maximum: 255}
 	validates :user_id, presence: true
 	belongs_to :user
+	has_one_attached :image
+
+	default_scope -> {order "created_at DESC"}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 	get "/contact", to: "pages#contact"
 	get "/about", to: "pages#about"
 	root "pages#index"
-	resources :users
+	resources :users, :posts
 	get "/signup", to: "users#new"
 
 	get "/login", to: "sessions#new"
