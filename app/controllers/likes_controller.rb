04349601ea2a9cb08e41ad1348287fc2afd5008a
deushka <ers98ers@gmail.com>
2018-06-25 15:30:03 +0600

    Added likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,12 @@
 class LikesController < ApplicationController
 
 	def create
+		@post_id = params[:like][:post_id]
+		u = Like.new(post_id: @post_id, user_id: current_user.id)
+		u.save
+		respond_to do |format|
+			format.js{}
+		end
 
 	end
 
